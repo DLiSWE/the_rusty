8e72257e90d5474d7e63852b8afc03b420c88692
Daywick <derekli11204@gmail.com>
2023-04-02 00:59:12 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+    the_rusty();
+}
+
+fn the_rusty() {
+    let temp = 5;
+    for i in 0..temp {
+        println!("This is the return value {}", i)
+    }
+}
+
